22ce9618457cc8b49e8088b61cb6c843ced29c5d
21NA1A0546 <21na1a0546.lingayas@limat.edu.in>
2024-12-29 07:25:51 +0530

    Add whitenoise for static files handling

--- weather_project/settings.py
+++ weather_project/settings.py
@@ -65,14 +65,6 @@ WSGI_APPLICATION = 'weather_project.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
-
-
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
 
